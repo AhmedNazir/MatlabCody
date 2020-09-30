5969fc6eb0329132025907235bdb424d683a70c4
Ahmed Nazir <AhmedNazir@users.noreply.github.com>
2020-09-30 15:28:27 +0600

    Update threeTimes.m

--- threeTimes.m
+++ threeTimes.m
@@ -1,30 +1,48 @@
+%%
+% Solution 1
+
 function y = threeTimes(x)
-x = sort(x);
-res = [];
+unique_x = unique(x);
 
-ii = 1;
-while ii<=length(x)
-    a = x(ii);
-    n = 0;
-    
-    while ii<=length(x)
-        if x(ii) == a
-            n = n + 1;
-            ii = ii + 1;
-            
-        else
-            break;
-        end
-        
+y = [];
+for ii = 1 : length(unique_x)
+    if sum(x == unique_x(ii)) == 3
+        y = [y unique_x(ii)];
     end
-    
-    if n==3
-        res = [res a];
-    end
-    
-    
 end
-y = res;
 
 end
 
+%%
+% Solution 2
+%
+%
+% function y = threeTimes(x)
+% x = sort(x);
+% res = [];
+%
+% ii = 1;
+% while ii<=length(x)
+%     a = x(ii);
+%     n = 0;
+%
+%     while ii<=length(x)
+%         if x(ii) == a
+%             n = n + 1;
+%             ii = ii + 1;
+%
+%         else
+%             break;
+%         end
+%
+%     end
+%
+%     if n==3
+%         res = [res a];
+%     end
+%
+%
+% end
+% y = res;
+%
+% end
