0635433b1fee0cce3f7b4ef18b1dc9a502c23729
Ahmed Nazir <AhmedNazir@users.noreply.github.com>
2020-10-01 12:54:27 +0600

    Update duplicates.m

--- duplicates.m
+++ duplicates.m
@@ -3,12 +3,6 @@ function dups = duplicates(strs)
 elements = unique(strs);
 frequency = countcats(categorical(strs));
 
-dups ={};
-for i = 1: length(elements)
-    if frequency(i) > 1
-        dups{ end + 1} = elements{i};
-    end
-end
-
+dups =elements(frequency>1);
 
 end
